bcce499930771b766ab76784965b5965a6e67b34
asokolov07011983 <holland48672@gmail.com>
2024-10-05 15:52:50 +0300

    add register page

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -2,10 +2,64 @@
 import { Layout } from "../../components/Layout";
 
 import styles from "./index.module.css";
+import {Card, Form, Row, Space, Typography} from "antd";
+import {CustomInput} from "../../components/CustomInput";
+import {PasswordInput} from "../../components/PasswordInput";
+import {CustomBtn} from "../../components/CustomBtn";
+import {Link} from "react-router-dom";
+import {Paths} from "../../paths";
 export const Register = () => {
     return (
         <Layout>
-            <p>Register</p>
+            <Row
+                align='middle'
+                justify={'center'}
+            >
+                <Card
+                    title='Зарегистрируйтесь'
+                    style={{
+                        width: '30rem'
+                    }}
+                >
+                    <Form
+                        onFinish={() => null}
+                    >
+                        <CustomInput
+                            name='name'
+                            placeholder='Имя'
+                        />
+                        <CustomInput
+                            type='email'
+                            name='email'
+                            placeholder='Email'
+                        />
+                        <PasswordInput
+                            name='password'
+                            placeholder='Пароль'
+                        />
+                        <PasswordInput
+                            name='confirmPassword'
+                            placeholder='Повторите пароль'
+                        />
+                        <CustomBtn
+                            type='primary'
+                            htmlType='submit'
+                            loading={false}
+                        >
+                            Зарегистрироваться
+                        </CustomBtn>
+                        <Space
+                            direction='vertical'
+                            size='large'
+                        >
+                            <Typography.Text>
+                                Уже зарегистрированы?
+                                <Link to={Paths.login}>Войти</Link>
+                            </Typography.Text>
+                        </Space>
+                    </Form>
+                </Card>
+            </Row>
         </Layout>
 
     )
